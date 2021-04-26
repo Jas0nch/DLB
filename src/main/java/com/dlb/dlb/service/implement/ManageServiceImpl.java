78bd9b463bd5f9dfa950e2db9e3a438a122061b2
Stan cao <stan.github@gmail.com>
2021-04-25 21:32:55 -0400

    connect remote docker and run image successfully

--- src/main/java/com/dlb/dlb/service/implement/ManageServiceImpl.java
+++ src/main/java/com/dlb/dlb/service/implement/ManageServiceImpl.java
@@ -2,8 +2,16 @@ package com.dlb.dlb.service.implement;
 
 import com.dlb.dlb.service.ManageService;
 import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.command.CreateContainerCmd;
+import com.github.dockerjava.api.command.CreateContainerResponse;
+import com.github.dockerjava.api.command.PullImageCmd;
+import com.github.dockerjava.api.model.ExposedPort;
+import com.github.dockerjava.api.model.HostConfig;
 import com.github.dockerjava.api.model.Info;
+import com.github.dockerjava.api.model.Ports;
+import com.github.dockerjava.api.model.Ports.Binding;
 import com.github.dockerjava.core.DockerClientBuilder;
+import com.github.dockerjava.core.command.CreateContainerCmdImpl;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -25,11 +33,28 @@ public class ManageServiceImpl implements ManageService {
     return false;
   }
 
+  static String imageName = "stangithubdocker/dlb-agent";
+
   @Override
   public boolean startNode(String ip) {
     DockerClient dockerClient = DockerClientBuilder.getInstance("tcp://" + host + ":2375").build();
     Info info = dockerClient.infoCmd().exec();
-    System.out.println(info);
-    return false;
+
+    dockerClient.pullImageCmd(imageName);
+
+    ExposedPort tcp8081 = ExposedPort.tcp(8081);
+
+    Ports portBindings = new Ports();
+    portBindings.bind(tcp8081, Binding.bindPort(8080));
+
+    CreateContainerResponse container = dockerClient.createContainerCmd(imageName).withCmd("-d")
+        .withExposedPorts(tcp8081)
+        .withHostConfig(new HostConfig()
+            .withPortBindings(portBindings)).exec();
+
+    dockerClient.startContainerCmd(container.getId()).exec();
+
+//    System.out.println(info);
+    return true;
   }
 }
